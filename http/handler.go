1da9ac478607efa1ea920dbc71f3223b62d6a0da
Rajesh Lingappa <rajesh.lingappa@gmail.com>
2018-02-20 11:01:31 +0800

    Added statsd and basic logging

---
+++ http/handler.go
@@ -0,0 +1,191 @@
+// handler
+package handler
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strconv"
+	"sync"
+	"time"
+
+	logrus "github.com/sirupsen/logrus"
+	"gopkg.in/alexcesaro/statsd.v2"
+)
+
+// handler contains the address of the main Target and the one for the Alternative target
+type Handler struct {
+	Target                string
+	Alternative           string
+	Randomizer            rand.Rand
+	Logger                *logrus.Entry
+	HttpStats             *statsd.Client
+	HttpStatsPri          *statsd.Client
+	HttpStatsAlt          *statsd.Client
+	Debug                 bool
+	AlternateTimeout      int
+	AlternateHostRewrite  bool
+	Percent               float64
+	ProductionTimeout     int
+	ProductionHostRewrite bool
+}
+
+var mutex = &sync.Mutex{}
+
+func forwardToAlternate(h Handler, alternativeRequest *http.Request) {
+	defer func() {
+		if r := recover(); r != nil && h.Debug {
+			h.Logger.Warn("Recovered in forwardToAlternate", r)
+		}
+	}()
+
+	t := h.HttpStatsAlt.NewTiming()
+	// Open new TCP connection to the server
+	clientTcpConn, err := net.DialTimeout("tcp", h.Alternative, time.Duration(time.Duration(h.AlternateTimeout)*time.Second))
+	if err != nil {
+		if h.Debug {
+			h.Logger.WithFields(logrus.Fields{
+				"host": h.Alternative,
+			}).Warn("Failed to connect to alternate backend.")
+		}
+		h.HttpStatsAlt.Increment(strconv.Itoa(http.StatusServiceUnavailable))
+		return
+	}
+
+	clientHttpConn := httputil.NewClientConn(clientTcpConn, nil) // Start a new HTTP connection on it
+	defer clientHttpConn.Close()                                 // Close the connection to the server
+	if h.AlternateHostRewrite {
+		alternativeRequest.Host = h.Alternative
+	}
+
+	err = clientHttpConn.Write(alternativeRequest) // Pass on the request
+	if err != nil {
+		if h.Debug {
+			h.Logger.Warnf("Failed to send to alternate backend: %s: %v", h.Alternative, err)
+		}
+		return
+	}
+
+	resp, err := clientHttpConn.Read(alternativeRequest) // Read back the reply
+	if err != nil && err != httputil.ErrPersistEOF {
+		if h.Debug {
+			h.Logger.Warnf("Failed to receive from alternate backend: %s: %v", h.Alternative, err)
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	t.Send("latency")
+	h.HttpStatsAlt.Increment(strconv.Itoa(resp.StatusCode))
+}
+
+// ServeHTTP duplicates the incoming request (req) and does the request to the Target
+// and the Alternate target discading the Alternate response
+func (h Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var productionRequest, alternativeRequest *http.Request
+	h.HttpStats.Increment("req")
+
+	mutex.Lock()
+	rnd := h.Randomizer.Float64()
+	mutex.Unlock()
+
+	if h.Percent == 100.0 || rnd*100 < h.Percent { //h.Randomizer
+		alternativeRequest, productionRequest = DuplicateRequest(req)
+		go forwardToAlternate(h, alternativeRequest)
+	} else {
+		productionRequest = req
+	}
+
+	defer func() {
+		if r := recover(); r != nil && h.Debug {
+			h.Logger.Info("Recovered in ServeHTTP", r)
+		}
+	}()
+
+	t := h.HttpStatsPri.NewTiming()
+	// Open new TCP connection to the server
+	clientTcpConn, err := net.DialTimeout("tcp", h.Target, time.Duration(time.Duration(h.ProductionTimeout)*time.Second))
+	if err != nil {
+		h.Logger.WithFields(logrus.Fields{
+			"host": h.Target,
+		}).Error("Failed to connect to primary backend.")
+		w.WriteHeader(http.StatusServiceUnavailable)
+		h.HttpStatsPri.Increment(strconv.Itoa(http.StatusServiceUnavailable))
+		return
+	}
+
+	clientHttpConn := httputil.NewClientConn(clientTcpConn, nil) // Start a new HTTP connection on it
+	defer clientHttpConn.Close()                                 // Close the connection to the server
+
+	if h.ProductionHostRewrite {
+		productionRequest.Host = h.Target
+	}
+
+	err = clientHttpConn.Write(productionRequest) // Pass on the request
+	if err != nil {
+		h.Logger.Errorf("Failed to send to %s: %v", h.Target, err)
+		return
+	}
+
+	resp, err := clientHttpConn.Read(productionRequest) // Read back the reply
+	if err != nil && err != httputil.ErrPersistEOF {
+		h.Logger.Errorf("Failed to receive from %s: %v", h.Target, err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	for k, v := range resp.Header {
+		w.Header()[k] = v
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	body, _ := ioutil.ReadAll(resp.Body)
+	w.Write(body)
+	t.Send("latency")
+	h.HttpStatsPri.Increment(strconv.Itoa(resp.StatusCode))
+
+}
+
+func DuplicateRequest(request *http.Request) (request1 *http.Request, request2 *http.Request) {
+	b2 := new(bytes.Buffer)
+	b1 := new(bytes.Buffer)
+	w := io.MultiWriter(b1, b2)
+	io.Copy(w, request.Body)
+	defer request.Body.Close()
+	request1 = &http.Request{
+		Method:        request.Method,
+		URL:           request.URL,
+		Proto:         request.Proto,
+		ProtoMajor:    request.ProtoMajor,
+		ProtoMinor:    request.ProtoMinor,
+		Header:        request.Header,
+		Body:          nopCloser{b1},
+		Host:          request.Host,
+		ContentLength: request.ContentLength,
+		Close:         true,
+	}
+	request2 = &http.Request{
+		Method:        request.Method,
+		URL:           request.URL,
+		Proto:         request.Proto,
+		ProtoMajor:    request.ProtoMajor,
+		ProtoMinor:    request.ProtoMinor,
+		Header:        request.Header,
+		Body:          nopCloser{b2},
+		Host:          request.Host,
+		ContentLength: request.ContentLength,
+		Close:         true,
+	}
+	return
+}
+
+type nopCloser struct {
+	io.Reader
+}
+
+func (nopCloser) Close() error { return nil }
--- teeproxy.go
+++ teeproxy.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"crypto/tls"
 	"flag"
-	"io"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
 	"net/http"
-	"net/url"
+	"os"
 	"runtime"
 	"time"
+
+	proxy "./http"
+
+	logrus "github.com/sirupsen/logrus"
+	"gopkg.in/alexcesaro/statsd.v2"
 )
 
 // Console flags
@@ -21,199 +23,98 @@ var (
 	targetProduction      = flag.String("a", "localhost:8080", "where production traffic goes. http://localhost:8080/production")
 	altTarget             = flag.String("b", "localhost:8081", "where testing traffic goes. response are skipped. http://localhost:8081/test")
 	debug                 = flag.Bool("debug", false, "more logging, showing ignored output")
-	productionTimeout     = flag.Int("a.timeout", 2500, "timeout in milliseconds for production traffic")
-	alternateTimeout      = flag.Int("b.timeout", 1000, "timeout in milliseconds for alternate site traffic")
+	productionTimeout     = flag.Int("a.timeout", 3, "timeout in seconds for production traffic")
+	alternateTimeout      = flag.Int("b.timeout", 1, "timeout in seconds for alternate site traffic")
 	productionHostRewrite = flag.Bool("a.rewrite", false, "rewrite the host header when proxying production traffic")
 	alternateHostRewrite  = flag.Bool("b.rewrite", false, "rewrite the host header when proxying alternate site traffic")
 	percent               = flag.Float64("p", 100.0, "float64 percentage of traffic to send to testing")
 	tlsPrivateKey         = flag.String("key.file", "", "path to the TLS private key file")
 	tlsCertificate        = flag.String("cert.file", "", "path to the TLS certificate file")
+	logFile               = flag.String("log.file", "/tmp/teeproxy.log", "path to the log file")
+	serviceName           = flag.String("service.name", "unknown", "service name")
+	hostname              = flag.String("hostname", "localhost", "hostname")
+	statsdAddress         = flag.String("statsd.address", "localhost:8125", "statsd server address in the for host:port")
+	statsdPrefix          = flag.String("statsd.prefix", "teeproxy", "prefix to be used for metrics sent to statsd")
 )
-
-
-// Sets the request URL.
-//
-// This turns a inbound request (a request without URL) into an outbound request.
-func setRequestTarget(request *http.Request, target *string) {
-	URL, err := url.Parse("http://" + *target + request.URL.String())
-	if err != nil {
-		log.Println(err)
-	}
-	request.URL = URL
-}
-
-
-// Sends a request and returns the response.
-func handleRequest(request *http.Request, timeout time.Duration) (*http.Response) {
-	transport := &http.Transport{
-		// NOTE(girone): DialTLS is not needed here, because the teeproxy works
-		// as an SSL terminator.
-		Dial: (&net.Dialer{  // go1.8 deprecated: Use DialContext instead
-			Timeout: timeout,
-			KeepAlive: 10 * timeout,
-		}).Dial,
-		// Always close connections to the alternative and production servers.
-		DisableKeepAlives: true,
-		//IdleConnTimeout: timeout,  // go1.8
-		TLSHandshakeTimeout: timeout,
-		ResponseHeaderTimeout: timeout,
-		ExpectContinueTimeout: timeout,
-	}
-	// Do not use http.Client here, because it's higher level and processes
-	// redirects internally, which is not what we want.
-	//client := &http.Client{
-	//	Timeout: timeout,
-	//	Transport: transport,
-	//}
-	//response, err := client.Do(request)
-	response, err := transport.RoundTrip(request)
-	if err != nil {
-		log.Println("Request failed:", err)
-	}
-	return response
-}
-
-// handler contains the address of the main Target and the one for the Alternative target
-type handler struct {
-	Target      string
-	Alternative string
-	Randomizer  rand.Rand
-}
-
-// ServeHTTP duplicates the incoming request (req) and does the request to the
-// Target and the Alternate target discading the Alternate response
-func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	var productionRequest, alternativeRequest *http.Request
-	if *percent == 100.0 || h.Randomizer.Float64()*100 < *percent {
-		alternativeRequest, productionRequest = DuplicateRequest(req)
-		go func() {
-			defer func() {
-				if r := recover(); r != nil && *debug {
-					log.Println("Recovered in ServeHTTP(alternate request) from:", r)
-				}
-			}()
-
-			setRequestTarget(alternativeRequest, altTarget)
-
-			if *alternateHostRewrite {
-				alternativeRequest.Host = h.Alternative
-			}
-
-			timeout := time.Duration(*alternateTimeout) * time.Millisecond
-			// This keeps responses from the alternative target away from the outside world.
-			_ = handleRequest(alternativeRequest, timeout)
-		}()
-	} else {
-		productionRequest = req
-	}
-	defer func() {
-		if r := recover(); r != nil && *debug {
-			log.Println("Recovered in ServeHTTP(production request) from:", r)
-		}
-	}()
-
-	setRequestTarget(productionRequest, targetProduction)
-
-	if *productionHostRewrite {
-		productionRequest.Host = h.Target
-	}
-
-	timeout := time.Duration(*productionTimeout) * time.Millisecond
-	resp := handleRequest(productionRequest, timeout)
-
-	if resp != nil {
-		defer resp.Body.Close()
-
-		// Forward response headers.
-		for k, v := range resp.Header {
-			w.Header()[k] = v
-		}
-		w.WriteHeader(resp.StatusCode)
-
-		// Forward response body.
-		body, _ := ioutil.ReadAll(resp.Body)
-		w.Write(body)
-	}
-}
-
 
 func main() {
 	flag.Parse()
 
-	log.Printf("Starting teeproxy at %s sending to A: %s and B: %s",
-	           *listen, *targetProduction, *altTarget)
-
 	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	var err error
 
 	var listener net.Listener
 
+	//set up logging
+	fileHandle, err := os.OpenFile(*logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileHandle.Close()
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(fileHandle)
+
+	logger := logrus.WithFields(logrus.Fields{
+		"service_name": *serviceName,
+		"hostname":     *hostname,
+	})
+
+	//set up statsd client
+	log.Printf("%s, %s", statsd.Address(*statsdAddress), *statsdPrefix) //statsd.Address(*statsdAddress),
+	statsdClient, err := statsd.New(statsd.Prefix(*statsdPrefix))       // Connect to the UDP port 8125 by default.
+	if err != nil {
+		// If nothing is listening on the target port, an error is returned and
+		// the returned client does nothing but is still usable. So we can
+		// just log the error and go on.
+		log.Print(err)
+	}
+	defer statsdClient.Close()
+	//log.Printf("%+v", statsdClient)
+	httpStats := statsdClient.Clone(statsd.Prefix("http.in"))
+	httpStatsPri := statsdClient.Clone(statsd.Prefix("http.pri"))
+	httpStatsAlt := statsdClient.Clone(statsd.Prefix("http.alt"))
+
+	logger.Info("Starting teeproxy...")
+
+	//ssl stuff
 	if len(*tlsPrivateKey) > 0 {
 		cer, err := tls.LoadX509KeyPair(*tlsCertificate, *tlsPrivateKey)
 		if err != nil {
-			log.Fatalf("Failed to load certficate: %s and private key: %s", *tlsCertificate, *tlsPrivateKey)
+			logger.Errorf("Failed to load certficate: %s and private key: %s", *tlsCertificate, *tlsPrivateKey)
+			return
 		}
 
 		config := &tls.Config{Certificates: []tls.Certificate{cer}}
 		listener, err = tls.Listen("tcp", *listen, config)
 		if err != nil {
-			log.Fatalf("Failed to listen to %s: %s", *listen, err)
+			logger.Errorf("Failed to listen to %s: %s", *listen, err)
+			return
 		}
 	} else {
 		listener, err = net.Listen("tcp", *listen)
 		if err != nil {
-			log.Fatalf("Failed to listen to %s: %s", *listen, err)
+			logger.Errorf("Failed to listen to %s: %s", *listen, err)
+			return
 		}
 	}
 
-	h := handler{
-		Target:      *targetProduction,
-		Alternative: *altTarget,
-		Randomizer:  *rand.New(rand.NewSource(time.Now().UnixNano())),
+	//prepare the handler
+	h := proxy.Handler{
+		Target:                *targetProduction,
+		Alternative:           *altTarget,
+		Randomizer:            *rand.New(rand.NewSource(time.Now().UnixNano())),
+		Logger:                logger,
+		HttpStats:             httpStats,
+		HttpStatsPri:          httpStatsPri,
+		HttpStatsAlt:          httpStatsAlt,
+		Debug:                 *debug,
+		AlternateTimeout:      *alternateTimeout,
+		AlternateHostRewrite:  *alternateHostRewrite,
+		Percent:               *percent,
+		ProductionTimeout:     *productionTimeout,
+		ProductionHostRewrite: *productionHostRewrite,
 	}
 
-	server := &http.Server{
-		Handler: h,
-	}
-	server.Serve(listener)
-}
-
-type nopCloser struct {
-	io.Reader
-}
-
-func (nopCloser) Close() error { return nil }
-
-func DuplicateRequest(request *http.Request) (request1 *http.Request, request2 *http.Request) {
-	b1 := new(bytes.Buffer)
-	b2 := new(bytes.Buffer)
-	w := io.MultiWriter(b1, b2)
-	io.Copy(w, request.Body)
-	defer request.Body.Close()
-	request1 = &http.Request{
-		Method:        request.Method,
-		URL:           request.URL,
-		Proto:         request.Proto,
-		ProtoMajor:    request.ProtoMajor,
-		ProtoMinor:    request.ProtoMinor,
-		Header:        request.Header,
-		Body:          nopCloser{b1},
-		Host:          request.Host,
-		ContentLength: request.ContentLength,
-		Close:         true,
-	}
-	request2 = &http.Request{
-		Method:        request.Method,
-		URL:           request.URL,
-		Proto:         request.Proto,
-		ProtoMajor:    request.ProtoMajor,
-		ProtoMinor:    request.ProtoMinor,
-		Header:        request.Header,
-		Body:          nopCloser{b2},
-		Host:          request.Host,
-		ContentLength: request.ContentLength,
-		Close:         true,
-	}
-	return
+	logger.Info("Ready to serve")
+	http.Serve(listener, h)
 }
